6d298f85f803f518aea1ed6f279eb30c43d6c169
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-01-06 19:02:05 -0800

    All Done!! insert demo links

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 import "./work.css";
+import Works from "./Works";
 
 const Work = () => {
   return (
-   <section className="work section" id='portfolio'>
-<h2 className="section__title">Portfolio</h2>
-<span className="section__subtitle">Highlighted Projects</span>
-   </section>
-  )
-}
+    <section className="work section" id="portfolio">
+      <h2 className="section__title">Portfolio</h2>
+      <span className="section__subtitle">Highlighted Projects</span>
+      <Works />
+    </section>
+  );
+};
 
-export default Work
+export default Work;
